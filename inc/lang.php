924ec8f74bffe918bf9ea8cfd1cd50518aba777e
Tom <tom.aschmann@t-online.de>
2025-05-31 16:35:14 +0200

    Update lang.php

--- inc/lang.php
+++ inc/lang.php
@@ -1,18 +1,19 @@
 <?php
 // Sprachsteuerung für Rankifmy
 
-// 1. Sprachlogik mit Cookie-Save
+// 1. Sprachlogik mit Cookie-Save (nur Cookie setzen, wenn noch kein Output!)
 function getLanguage() {
     // Sprache per GET-Parameter (setzt Cookie für 1 Jahr)
     if (isset($_GET['lang']) && in_array($_GET['lang'], ['de', 'en'])) {
-        setcookie('lang', $_GET['lang'], time() + 365*24*60*60, '/');
+        // Nur Cookie setzen, wenn noch keine Header gesendet wurden:
+        if (!headers_sent()) {
+            setcookie('lang', $_GET['lang'], time() + 365*24*60*60, '/');
+        }
         return $_GET['lang'];
     }
-    // Cookie (vom User gesetzt)
     if (isset($_COOKIE['lang']) && in_array($_COOKIE['lang'], ['de', 'en'])) {
         return $_COOKIE['lang'];
     }
-    // Default
     return 'de';
 }
 
